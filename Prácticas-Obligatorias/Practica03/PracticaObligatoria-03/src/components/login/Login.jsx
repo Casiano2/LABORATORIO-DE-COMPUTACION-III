6fb45426ab6f36909f277e1672c24cf651fabefc
Casiano <casianoalmeida2004@hotmail.com>
2024-04-30 19:01:26 -0300

    Commit-Practica03

---
+++ Prácticas-Obligatorias/Practica03/PracticaObligatoria-03/src/components/login/Login.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+const Login = () => {
+    const [username, setUsername] = useState('');
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        const value = event.target.value;
+
+        if (value.toLowerCase().includes("o")) {
+
+
+            alert("El username no tiene que contener o")
+
+        } else {
+            setUsername(value);
+        };
+    }
+    
+    const handleRegister = () => {
+
+        if (username === "" || username.toLowerCase().includes("o")) {
+            alert("Usuario inválido");
+        } else {
+
+            alert("Ha ingresado correctamente")
+
+        };
+    }
+
+    return (
+
+        <form onSubmit={handleRegister}>
+            <label>
+                User Name:
+                <input type="text" name="name" onChange={handleChange} />
+            </label>
+            <button >Registrarse</button>
+            <p>{username}</p>
+        </form>
+    );
+};
+
+export default Login
+
+
